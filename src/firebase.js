71dda07566e44dee145bfc3661bcfefa03db7d34
Magalaweng Clive Mmakola <mammako022@student.wethinkcode.co.za>
2023-09-30 23:22:29 +0200

    Add Google authentication for client

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,33 @@
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
+import { GoogleAuthProvider } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: 'YOUR_API_KEY',
-//   authDomain: 'YOUR_AUTH_DOMAIN',
-//   projectId: 'YOUR_PROJECT_ID',
-//   storageBucket: 'YOUR_STORAGE_BUCKET',
-//   messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-//   appId: 'YOUR_APP_ID',
-// };
+const provider = new GoogleAuthProvider();
 
-// const app = firebase.initializeApp(firebaseConfig);
-// export const auth = app.auth();
-// export default app;
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+provider.setCustomParameters({
+    'login_hint': 'user@example.com'
+  });
+
+  import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+      
